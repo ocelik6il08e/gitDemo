182bbfb4ecce5195622650f029e7040ca25b7347
ocelik66il08e <usaonurcelik@gmail.com>
2020-08-01 11:53:10 -0400

    third time of first day

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,5 +2,11 @@ public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello B20");
 
+        for (int i = 0; i <= 5; i++) {
+
+            System.out.println("Today is Saturday");
+
+        }
+
     }
 }
